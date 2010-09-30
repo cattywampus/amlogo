c58853f505ea73825631f70608c3a4e5c416bd1c
Keith Walters <keith.walters@cattywamp.us>
2010-09-30 15:07:35 -0400

    Adjusted program to be full screen

--- amlogo.c
+++ amlogo.c
@@ -306,9 +306,10 @@ void TimerFunction(int value) {
 int main(int argc, char* argv[]) {
   glutInit(&argc, argv);
   glutInitDisplayMode(GLUT_RGBA | GLUT_DOUBLE | GLUT_DEPTH);
-  glutInitWindowSize(800, 600);
+  glutInitWindowSize(1024, 768);
   glutInitWindowPosition(100, 100);
   glutCreateWindow("Adaptive Methods");
+  glutFullScreen();
 
   init();
 
