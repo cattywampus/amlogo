e515e8be2a74e38a086ea13fac13c4c16d66abe2
Keith Walters <keith.walters@cattywamp.us>
2010-09-30 15:07:56 -0400

    Added second light and removed some specular highlighting

--- amlogo.c
+++ amlogo.c
@@ -182,25 +182,26 @@ static void init() {
 
   glEnable(GL_DEPTH_TEST);
 
-  GLfloat mat_specular[] = {0.5, 0.5, 0.5, 1.0};
-  GLfloat mat_shininess[] = {50.0};
   GLfloat mat_diffuse[] = {0.674, 0.0, 0.0, 1.0};
   GLfloat mat_ambient[] = {0.1, 0.1, 0.1, 1.0};
-  GLfloat light_position[] = {0.0, 5.0, 5.0, 0.0};
+  GLfloat light1_position[] = {-10.0, -10.0, 10.0, 0.0};
+  GLfloat light2_position[] = {10.0, 10.0, -10.0, 0.0};
   GLfloat white_light[] = {1.0, 1.0, 1.0, 1.0};
-  GLfloat lmodel_ambient[] = {0.5, 0.5, 0.5, 1.0};
+  GLfloat lmodel_ambient[] = {0.9, 0.9, 0.9, 1.0};
 
-  glMaterialfv(GL_FRONT_AND_BACK, GL_SPECULAR, mat_specular);
-  glMaterialfv(GL_FRONT_AND_BACK, GL_SHININESS, mat_shininess);
   glMaterialfv(GL_FRONT_AND_BACK, GL_DIFFUSE, mat_diffuse);
   glMaterialfv(GL_FRONT_AND_BACK, GL_AMBIENT, mat_ambient);
-  glLightfv(GL_LIGHT0, GL_POSITION, light_position);
+  glLightfv(GL_LIGHT0, GL_POSITION, light1_position);
   glLightfv(GL_LIGHT0, GL_DIFFUSE, white_light);
   glLightfv(GL_LIGHT0, GL_SPECULAR, white_light);
+  glLightfv(GL_LIGHT1, GL_POSITION, light2_position);
+  glLightfv(GL_LIGHT1, GL_DIFFUSE, white_light);
+  glLightfv(GL_LIGHT1, GL_SPECULAR, white_light);
   glLightModelfv(GL_LIGHT_MODEL_AMBIENT, lmodel_ambient);
 
   glEnable(GL_LIGHTING);
   glEnable(GL_LIGHT0);
+  glEnable(GL_LIGHT1);
   glEnable(GL_POLYGON_SMOOTH);
 }
 
